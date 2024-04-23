adc2c6c3d80b0a2326208b0e2ac29777a1a30086
alonzo-bachelier <alonzo.bachelier@laplateforme.io>
2024-04-23 22:03:42 +0200

    changement du style des boutons pour des btn provenant de bootstrap, modifications du code existant pour ne pas empecher le responsive bootstrap, modification de la taille des cartes

--- javascript/script.js
+++ javascript/script.js
@@ -48,16 +48,17 @@ if (url.pathname.includes("films.html")) {
         let contenuHTML = "";
         for (let i = 0; i < filmsResultat.length; i++) {
             contenuHTML += `
-            <a class="card mb-3 lienCard" style="max-width: 540px; height:275px;" href="details.html?id=${filmsResultat[i].id}" target="_blank" >
+            <a class="card mb-3 lienCard" style="max-width: 450px; " href="details.html?id=${filmsResultat[i].id}" target="_blank" >
                 <div class="row g-0">
-                    <div class="col-md-4">
-                        <img src="https://image.tmdb.org/t/p/w500/${filmsResultat[i].poster_path}" class="img-fluid rounded-start" alt="image promotionnelle">
-                    </div>
+                    <div class="col-md-4 notes-img">
+                        <img src="https://image.tmdb.org/t/p/w500/${filmsResultat[i].poster_path}" class="img-fluid rounded-start images-cartes" alt="image promotionnelle">
+                        <small class="notes">Notes : ${filmsResultat[i].vote_average}</small>
+                        </div>
                     <div class="col-md-8">
                         <div class="card-body">
                             <h5 class="card-title">${filmsResultat[i].title}</h5>
                             <p class="card-text description">Description : ${filmsResultat[i].overview}</p>
-                            <small>Notes : ${filmsResultat[i].vote_average}</small>
+                            
                         </div>
                     </div>
                 </div>
@@ -87,7 +88,7 @@ if (url.pathname.includes("films.html")) {
     //script page films !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     afficherFilms("popular", 1);
     mettreAJourPagination();
-    populaire.classList.add("bouton-categorie-active");
+    populaire.classList.add("active");
 
     document.getElementById("page-precedente").addEventListener("click", () => {
         if (pageActuelle > 1) {
@@ -120,10 +121,10 @@ if (url.pathname.includes("films.html")) {
     });
 
     populaire.addEventListener("click", () => {
-        populaire.classList.add("bouton-categorie-active");
-        mieuxNotes.classList.remove("bouton-categorie-active");
-        prochainement.classList.remove("bouton-categorie-active");
-        enCours.classList.remove("bouton-categorie-active");
+        populaire.classList.add("active");
+        mieuxNotes.classList.remove("active");
+        prochainement.classList.remove("active");
+        enCours.classList.remove("active");
         categorieActuelle = "popular";
         pageActuelle = 1;
         afficherFilms(categorieActuelle, pageActuelle);
@@ -131,10 +132,10 @@ if (url.pathname.includes("films.html")) {
     });
 
     mieuxNotes.addEventListener("click", () => {
-        populaire.classList.remove("bouton-categorie-active");
-        mieuxNotes.classList.add("bouton-categorie-active");
-        prochainement.classList.remove("bouton-categorie-active");
-        enCours.classList.remove("bouton-categorie-active");
+        populaire.classList.remove("active");
+        mieuxNotes.classList.add("active");
+        prochainement.classList.remove("active");
+        enCours.classList.remove("active");
         categorieActuelle = "top_rated";
         pageActuelle = 1;
         afficherFilms(categorieActuelle, pageActuelle);
@@ -142,10 +143,10 @@ if (url.pathname.includes("films.html")) {
     });
 
     prochainement.addEventListener("click", () => {
-        populaire.classList.remove("bouton-categorie-active");
-        mieuxNotes.classList.remove("bouton-categorie-active");
-        prochainement.classList.add("bouton-categorie-active");
-        enCours.classList.remove("bouton-categorie-active");
+        populaire.classList.remove("active");
+        mieuxNotes.classList.remove("active");
+        prochainement.classList.add("active");
+        enCours.classList.remove("active");
         categorieActuelle = "upcoming";
         pageActuelle = 1;
         afficherFilms(categorieActuelle, pageActuelle);
@@ -153,10 +154,10 @@ if (url.pathname.includes("films.html")) {
     });
 
     enCours.addEventListener("click", () => {
-        populaire.classList.remove("bouton-categorie-active");
-        mieuxNotes.classList.remove("bouton-categorie-active");
-        prochainement.classList.remove("bouton-categorie-active");
-        enCours.classList.add("bouton-categorie-active");
+        populaire.classList.remove("active");
+        mieuxNotes.classList.remove("active");
+        prochainement.classList.remove("active");
+        enCours.classList.add("active");
         categorieCourante = "now_playing";
         pageActuelle = 1;
         afficherFilms(categorieCourante, pageActuelle);
@@ -206,16 +207,17 @@ if (url.pathname.includes("series.html")) {
         let contenuHTML = "";
         for (let i = 0; i < seriesResultat.length; i++) {
             contenuHTML += `
-            <div class="card mb-3" style="max-width: 540px; height:275px;">
+            <div class="card mb-3" style="max-width: 450px;">
                 <div class="row g-0">
-                    <div class="col-md-4" data-tv-id="${seriesResultat[i].id}">
-                        <img src="https://image.tmdb.org/t/p/w500/${seriesResultat[i].poster_path}" class="img-fluid rounded-start" alt="image promotionnelle">
-                    </div>
+                    <div class="col-md-4 notes-img" data-tv-id="${seriesResultat[i].id}">
+                        <img src="https://image.tmdb.org/t/p/w500/${seriesResultat[i].poster_path}" class="img-fluid rounded-start images-cartes" alt="image promotionnelle">
+                        <small class="notes">Notes : ${seriesResultat[i].vote_average}</small>
+                        </div>
                     <div class="col-md-8">
                         <div class="card-body">
                             <h5 class="card-title">${seriesResultat[i].name}</h5>
                             <p class="card-text description">Description : ${seriesResultat[i].overview}</p>
-                            <small>Notes : ${seriesResultat[i].vote_average}</small>
+                            
                         </div>
                     </div>
                 </div>
@@ -244,7 +246,7 @@ if (url.pathname.includes("series.html")) {
     }
     // SCRIPT DE LA PAGE SERIES
     afficherSeries("popular", 1);
-    populaire.classList.add("bouton-categorie-active");
+    populaire.classList.add("active");
 
     document.getElementById("page-precedente").addEventListener("click", () => {
         if (pageActuelle > 1) {
@@ -277,10 +279,10 @@ if (url.pathname.includes("series.html")) {
     });
 
     populaire.addEventListener("click", () => {
-        populaire.classList.add("bouton-categorie-active");
-        mieuxNotes.classList.remove("bouton-categorie-active");
-        enDirect.classList.remove("bouton-categorie-active");
-        aSuivre.classList.remove("bouton-categorie-active");
+        populaire.classList.add("active");
+        mieuxNotes.classList.remove("active");
+        enDirect.classList.remove("active");
+        aSuivre.classList.remove("active");
         categorieActuelle = "popular";
         pageActuelle = 1;
         afficherSeries(categorieActuelle, pageActuelle);
@@ -288,10 +290,10 @@ if (url.pathname.includes("series.html")) {
     });
 
     mieuxNotes.addEventListener("click", () => {
-        populaire.classList.remove("bouton-categorie-active");
-        mieuxNotes.classList.add("bouton-categorie-active");
-        enDirect.classList.remove("bouton-categorie-active");
-        aSuivre.classList.remove("bouton-categorie-active");
+        populaire.classList.remove("active");
+        mieuxNotes.classList.add("active");
+        enDirect.classList.remove("active");
+        aSuivre.classList.remove("active");
         categorieActuelle = "top_rated";
         pageActuelle = 1;
         afficherSeries(categorieActuelle, pageActuelle);
@@ -299,10 +301,10 @@ if (url.pathname.includes("series.html")) {
     });
 
     enDirect.addEventListener("click", () => {
-        populaire.classList.remove("bouton-categorie-active");
-        mieuxNotes.classList.remove("bouton-categorie-active");
-        enDirect.classList.add("bouton-categorie-active");
-        aSuivre.classList.remove("bouton-categorie-active");
+        populaire.classList.remove("active");
+        mieuxNotes.classList.remove("active");
+        enDirect.classList.add("active");
+        aSuivre.classList.remove("active");
         categorieActuelle = "on_the_air";
         pageActuelle = 1;
         afficherSeries(categorieActuelle, pageActuelle);
@@ -310,10 +312,10 @@ if (url.pathname.includes("series.html")) {
     });
 
     aSuivre.addEventListener("click", () => {
-        populaire.classList.remove("bouton-categorie-active");
-        mieuxNotes.classList.remove("bouton-categorie-active");
-        enDirect.classList.remove("bouton-categorie-active");
-        aSuivre.classList.add("bouton-categorie-active");
+        populaire.classList.remove("active");
+        mieuxNotes.classList.remove("active");
+        enDirect.classList.remove("active");
+        aSuivre.classList.add("active");
         categorieCourante = "airing_today";
         pageActuelle = 1;
         afficherSeries(categorieCourante, pageActuelle);
